d469d71d51dff1d3152d81a9a3e36cf5266a5a38
Hamish Starling <37508609+starswap@users.noreply.github.com>
2021-08-26 18:20:53 +0100

    Update outputs.cpp

--- outputs.cpp
+++ outputs.cpp
@@ -43,3 +43,25 @@ ostream& operator << (ostream& o, multiset<T> v) {
   o << "}";
   return o;
 }
+
+
+template <typename T,typename U>
+ostream& operator << (ostream& o, unordered_map<T,U> m) {
+  o << "{";
+  for (auto it = m.begin();it != m.end();it++) {
+     o << *it << ",";
+  }
+  o << "}";
+  return o;
+}
+
+
+template <typename T,typename U>
+ostream& operator << (ostream& o, map<T,U> m) {
+  o << "{";
+  for (auto it = m.begin();it != m.end();it++) {
+     o << *it << ",";
+  }
+  o << "}";
+  return o;
+}
