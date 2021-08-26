a7ac3e4ab8911c4778f26db6daf503a1a69ed099
Hamish Starling <37508609+starswap@users.noreply.github.com>
2021-08-26 12:40:52 +0100

    Update codeJamHackerCupTemplate.cpp

--- codeJamHackerCupTemplate.cpp
+++ codeJamHackerCupTemplate.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+  
+  //May remove in case of interactive problem or need to use c streams
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
   int T;
   cin >> T;
   for (int t = 0;t<T;t++) {
-    cout << "Case #" << t+1 << ": " << endl;
+    cout << "Case #" << t+1 << ": " << '\n';
   }
 }
