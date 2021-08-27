5be0c78252959d3ac1a5f7fc44479b88e00a1369
Hamish Starling <37508609+starswap@users.noreply.github.com>
2021-08-27 11:31:47 +0100

    Update basicFenwickTree.cpp

--- basicFenwickTree.cpp
+++ basicFenwickTree.cpp
@@ -38,3 +38,36 @@ class FenwickTree {
     inline T LSOne (T i) {return i&(-i);}
     vector<T> ft;
 };
+
+
+//Supporting point update, point query, range query but not range update.
+class FenwickTree {
+  private:
+    inline int LSOne(int S) {return S&(-S);}
+    vector<int> ft;
+    vector<int> pointQueries;
+  public:
+    FenwickTree(int n) {
+      ft.assign(n+1,0);
+      pointQueries.assign(n+1,0);
+    }
+    void update(int i,int v) {
+      pointQueries[i] += v;
+      for (;i<ft.size();i+= LSOne(i)) {
+        ft[i] += v;
+      }
+    }
+    int rsq(int j) {
+      int acc = 0;
+      for (;j>0;j-=LSOne(j)) {
+        acc += ft[j];
+      }
+      return acc;
+    }
+    int rsq(int i, int j) {
+      return rsq(j) - rsq(i-1);
+    }
+    int pointQuery(int i) {
+      return pointQueries[i];
+    }
+};
