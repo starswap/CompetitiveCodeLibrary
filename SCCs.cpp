eb5637ad34e1ad4a0c9341798dd27bb2810e2c10
Hamish Starling <37508609+starswap@users.noreply.github.com>
2021-09-01 17:40:31 +0100

    Create SCCs.cpp

---
+++ SCCs.cpp
@@ -0,0 +1,41 @@
+vector<bool> visited;
+vector<int> postOrder;
+void dfs(vector<vector<int>>&AL,int u) {
+  visited[u] = true;
+  for (int v : AL[u]) {
+    if (!visited[v]) 
+      dfs(AL,v);
+  }
+  postOrder.push_back(u);
+}
+
+template <typename T>
+ostream& operator << (ostream& o, vector<T> v) {
+  o << "[";
+  for (auto a : v) {
+    o << a << ",";
+  }
+  o << "]";
+  return o;
+}
+
+int Kosaraju(vector<vector<int>>&AL,vector<vector<int>> &AL_T) {
+  int n = AL.size();
+  visited.assign(n,false);
+  postOrder.clear();
+
+  for (int i=0;i<n;i++) {
+    if (!visited[i]) {
+      dfs(AL,i);
+    }
+  }
+  visited.assign(n,false);
+  int SCCs = 0;
+  for (int i=n-1;i>=0;i--) {
+    if (!visited[postOrder[i]]) {
+      dfs(AL_T,postOrder[i]);
+      SCCs++;
+    }
+  }
+  return SCCs;
+}
